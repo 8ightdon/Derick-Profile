99d8dbcbf88e80ac38012279e90ef56ff780dc0f
Elias Derick Orwa <167477484+8ightdon@users.noreply.github.com>
2025-04-04 23:58:31 +0300

    Done
    completed

---
+++ script.js
@@ -0,0 +1,7 @@
+/* script.js */
+document.addEventListener("DOMContentLoaded", () => {
+    const ctaButton = document.querySelector(".cta");
+    ctaButton.addEventListener("click", () => {
+        alert("Welcome to my portfolio!");
+    });
+});
